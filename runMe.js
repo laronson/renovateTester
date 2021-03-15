677a1dcd0745b2ae3fcbf90829bb83a994ba42fa
laronson <laronson21@gmail.com>
2021-03-15 14:42:56 -0700

    Adding outdated lodash

--- runMe.js
+++ runMe.js
@@ -1,4 +1,13 @@
 const isOdd = require("is-odd");
+const _ = require("lodash")
 
 console.log("hello world");
 console.log(isOdd(2));
+
+
+var users = [
+  { 'user': 'barney', 'age': 36 },
+  { 'user': 'fred',   'age': 40 }
+];
+
+console.log(_.pluck(users, "user"))
